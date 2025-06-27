42af3acc4da4d166018951ce6d17a3fe2568c8d7
MythEclipse <asepharyanalm001@gmail.com>
2025-06-27 15:36:02 +0700

    Add login and main stylesheets for improved UI design
    - Created a new login.css file to style the login page with a modern layout, including a background image, input fields, and buttons.
    - Established a new style.css file for the main application, incorporating responsive design, custom scrollbar styles, and various UI components such as headers, buttons, and forms.
    - Implemented media queries for better adaptability on different screen sizes.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,55 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 window.Alpine = Alpine;
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".home-slider", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        initialSlide: 1,
+        coverflowEffect: {
+            rotate: 20,    // Tetap dengan rotasi untuk efek miring
+            stretch: -20,  // <--- UBAH INI: Berikan nilai negatif untuk membuat slide berdempet
+            depth: 250,    // Tetap dengan kedalaman untuk efek "di belakang"
+            modifier: 1,   // Tetap dengan modifier
+            slideShadows: true,
+        },
+        loop: true,
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+        },
+    });
 
-Alpine.start();
+    var swiper2 = new Swiper(".review-slider", {
+        slidesPerView: 1,
+        grabCursor: true,
+        loop: true,
+        centeredSlides: true,
+        initialSlide: 1,
+        spaceBetween: 10,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            700: {
+                slidesPerView: 2,
+            },
+            1050: {
+                slidesPerView: 3,
+            },
+        },
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+        }
+    });
+});
