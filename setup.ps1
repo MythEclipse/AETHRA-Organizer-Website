fcb177b123ae6c5fdf4b0bab5ef77a5bcc8ea277
MythEclipse <asepharyanalm001@gmail.com>
2025-06-30 08:48:01 +0700

    feat: Enhance setup script with cache and config clearing commands

--- setup.ps1
+++ setup.ps1
@@ -4,3 +4,7 @@ npm run build
 php artisan migrate --seed
 php artisan storage:link
 php artisan key:generate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
